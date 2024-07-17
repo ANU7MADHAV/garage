d1a2432986269ae04f10d979d4a6455184ec895e
ANU7MADHAV <anumadhavan888@gmail>
2024-07-17 23:47:35 +0530

    small fix

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,7 +5,7 @@ import LoginImage from "../components/LoginImage";
 const LoginPage = () => {
   return (
     <div className="md:flex items-center h-screen">
-      <div className="flex-1">
+      <div className="flex-1 flex justify-center">
         <LoginImage />
       </div>
       <div className="h-full w-0.5 bg-gray-300 hidden md:block" />
